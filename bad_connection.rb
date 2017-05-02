15a7b3365ef115b4c886f9912a45a0696ab4d5e6
Jim Szalewski <jim.szalewski@gmaiil.com>
2017-05-02 09:22:47 -0600

    Delete bad_connection.rb

--- bad_connection.rb
+++
@@ -1,7 +0,0 @@
-# print the greeting "HELLO, THIS IS A GROCERY STORE!"
-# get input from the user and make sure its a string
-# if the input isn't in all caps print "I AM HAVING A HARD TIME HEARING YOU."
-# if the input IS in all caps, print "NO, THIS IS NOT A PET STORE."
-# if the input is empty print "HELLO?!"
-# first time input is "GOODBYE!" print "ANYTHING ELSE I CAN HELP WITH?"
-# if consecutively after that the input is "GOODBYE!" print "THANK YOU FOR CALLING!"
